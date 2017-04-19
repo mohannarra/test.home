39e9bbaeae805382fef78c9807f2c4723878d436
mohan narra <mohan narra>
2017-04-19 23:33:04 +0530

    changed value to values

--- FirstJava.java
+++ FirstJava.java
@@ -1,1 +1,16 @@
 
+package com.enums;
+
+public enum Currency {
+       PEENY(1),DICE(5),NICKLE(15),QUARTER(25);
+	
+	private int values;
+	
+	Currency(int values){
+		this.values = values;
+	}
+       
+	public int getValues(){
+		return values;
+	}
+}
